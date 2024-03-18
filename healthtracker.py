59db561efb32063bf32e29541b1ba0e618efa77d
daniel-dunning <68135537+daniel-dunning@users.noreply.github.com>
2024-03-18 11:48:59 -0400

    Update healthtracker.py
    updated the measurement time to use dateutil.parser....

--- healthtracker.py
+++ healthtracker.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from datetime import datetime
+from dateutil import parser
 import json
 
 app = FastAPI()
@@ -60,7 +61,8 @@ async def add_measurement(request: Request,
                           pulse: int = Form(None),
                           fluid_intake: int = Form(None),
                           time: str = Form(...)):
-    measurement_time = datetime.strptime(time, "%Y-%m-%d %H:%M:%S") if time else None
+    measurement_time = parser.parse(time)                          
+    # measurement_time = datetime.strptime(time, "%Y-%m-%d %H:%M:%S") if time else None
     tracker.add_measurement(weight, glucose, long_acting_insulin, short_acting_insulin, systolic, diastolic, pulse, fluid_intake, measurement_time)
     return {"message": "Measurement added successfully."}
 
